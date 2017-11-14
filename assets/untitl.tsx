f8282cfd9f504ad14870d72863cbaeda4c2ab465
p13sidi <despoina.sidiropoulou@gmail.com>
2017-11-14 15:46:03 +0200

    Delete untitl.tsx

--- assets/untitl.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="super_mario_tiles.png" tilewidth="16" tileheight="16" tilecount="350" columns="14">
- <image source="Πολυμέσα/super_mario_tiles.png.png" width="224" height="400"/>
-</tileset>
